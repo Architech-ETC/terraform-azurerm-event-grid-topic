27048244526adb814af8624bb5029e6556ff80ce
rachitha <87133789+rrachitha@users.noreply.github.com>
2021-08-31 12:42:17 -0400

    feat: Add event subscriptions resource (#1)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY THE AZURE EVENT GRID TOPIC
+# DEPLOY THE AZURE EVENT GRID TOPIC AND EVENT SUBSCRIPTIONS
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "azurerm_eventgrid_topic" "event-grid-topic" {
@@ -9,4 +9,10 @@ resource "azurerm_eventgrid_topic" "event-grid-topic" {
   resource_group_name = var.resource_group_name
 
   tags = lookup(each.value, "tags", {})
+}
+
+resource "azurerm_eventgrid_event_subscription" "event-subscription" {
+  for_each = { for sub in var.event_subscriptions : sub.name => sub }
+  name     = format("%s%s", lookup(each.value, "name"), var.environment)
+  scope    = azurerm_eventgrid_topic.event-grid-topic[each.key].id
 }
